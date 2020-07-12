1adf438b48c85ba87d492db31642d7f43b920aa4
luis briceño <siulbriceno20@gmail.com>
2020-07-12 12:50:13 -0400

    segunda prueba produccion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Luis Briceño</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
          <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!--<title>{{ config('app.name', 'Laravel') }}</title>-->
+        <!--<title>{{ config('app.name', 'Luis-briceño') }}</title>-->
 
         <!-- Scripts -->
         <!--<script src="{{ asset('js/app.js') }}" defer></script>-->
